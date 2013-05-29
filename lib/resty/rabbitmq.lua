73cf60a47568fbec56e2b7b33a8bef2006a6b722
Rohit Yadav <rohit.yadav@wingify.com>
2013-05-30 04:14:18 +0530

    lib: lua-resty rabbitmq lib that works on stomp
    Signed-off-by: Rohit Yadav <rohit.yadav@wingify.com>

--- lib/resty/rabbitmq.lua
+++ lib/resty/rabbitmq.lua
@@ -1,16 +1,195 @@
--- lua-resty-rabbitmq: Client library for implementing producers and consumers
--- for RabbitMQ which communicate with the broker over STOMP v1.2 using ngx
--- cosocket api.
+-- lua-resty-rabbitmq: Opinionated RabbitMQ STOMP v1.2 client lib
 -- Copyright (C) 2013 Rohit Yadav (bhaisaab), Wingify
 -- Opensourced at Wingify in New Delhi under the MIT License
 
 local tcp = ngx.socket.tcp
+local len = string.len
+local concat = table.concat
+local setmetatable = setmetatable
+local error = error
+local ngx = require "ngx"
 
 module(...)
 
-_VERSION = "0.01"
+_VERSION = "0.1"
 
+local STATE_CONNECTED = 1
+local STATE_COMMAND_SENT = 2
 local mt = { __index = _M }
+
+
+function new(self)
+    local sock, err = tcp()
+    if not sock then
+        return nil, err
+    end
+    return setmetatable({ sock = sock }, mt)
+end
+
+
+function set_timeout(self, timeout)
+    local sock = self.sock
+    if not sock then
+        --ngx.log(ngx.ERR, "TIMEOUT ERROR")
+        return nil, "not initialized"
+    end
+
+    return sock:settimeout(timeout)
+end
+
+
+function _login(self, user, passwd, vhost)
+    local sock = self.sock
+    if not sock then
+        return nil, "not initialized"
+    end
+
+    local req = "CONNECT\x0d\x0a" ..
+                "accept-version:1.2\x0d\x0a" ..
+                "login:" .. user .. "\x0d\x0a" ..
+                "passcode:" .. passwd .. "\x0d\x0a" ..
+                "host:" .. vhost .. "\x0d\x0a" ..
+                "\x0d\x0a\x00\x0d\x0a"
+    local ok, err = sock:send(req)
+    if not ok then
+        return ok, err
+    end
+
+    self.state = STATE_CONNECTED
+    -- FIXME: Check CONNECTION frame for errors etc.
+    local resp = sock:receiveuntil("\x00")
+    local data, err, partial = resp()
+    return ok, data
+end
+
+
+function connect(self, opts)
+    local sock = self.sock
+    if not sock then
+        return nil, "not initialized"
+    end
+
+    local host = opts.host
+    if not host then
+        host = "127.0.0.1"
+    end
+
+    local port = opts.port
+    if not port then
+        port = 61613  -- stomp port
+    end
+
+    local username = opts.username
+    if not username then
+        username = "guest"
+    end
+
+    local password = opts.password
+    if not password then
+        password = "guest"
+    end
+
+    local vhost = opts.vhost
+    if not vhost then
+        vhost = "/"
+    end
+
+    local pool = opts.pool
+    if not pool then
+        pool = concat({username, vhost, host, port}, ":")
+    end
+
+    local ok, err = sock:connect(host, port, { pool = pool })
+    if not ok then
+        return nil, "failed to connect: " .. err
+    end
+
+    local reused = sock:getreusedtimes()
+    if reused and reused > 0 then
+        self.state = STATE_CONNECTED
+        return 1
+    end
+
+    return _login(self, username, password, vhost)
+end
+
+
+function send(self, msg, exchange, binding, app_id, persistence, content_type)
+    local sock = self.sock
+    if not sock then
+        return nil, "not initialized"
+    end
+
+    -- FIXME: Implement a generator for creating response from a dictionary
+    local req = "SEND\x0d\x0a" ..
+                "destination:/exchange/" ..
+                    exchange .. "/" ..
+                    binding .. "\x0d\x0a" ..
+                "app-id:" .. app_id  .. "\x0d\x0a" ..
+                "receipt:m123\x0d\x0a" ..
+                "persistent:" .. persistence  .. "\x0d\x0a" ..
+                "content-type:" .. content_type .. "\x0d\x0a" ..
+                "\x0d\x0a" ..
+                msg .. "\x0d\x0a\x0d\x0a\x00\x0d\x0a"
+    return sock:send(req)
+end
+
+
+function confirm(self)
+    local sock = self.sock
+    if not sock then
+        return nil, "not initialized"
+    end
+
+    local resp = sock:receiveuntil("\x00")
+    local data, err, partial = resp()
+    if data ~= nil then
+        return 1, data
+    else
+        return nil, err
+    end
+end
+
+
+function set_keepalive(self, ...)
+    local sock = self.sock
+    if not sock then
+        return nil, "not initialized"
+    end
+
+    if self.state ~= STATE_CONNECTED then
+        return nil, "cannot be reused in the current connection state: "
+                    .. (self.state or "nil")
+    end
+
+    self.state = nil
+    return sock:setkeepalive(...)
+end
+
+
+function get_reused_times(self)
+    local sock = self.sock
+    if not sock then
+        return nil, "not initialized"
+    end
+
+    return sock:getreusedtimes()
+end
+
+
+function close(self)
+    local sock = self.sock
+    if not sock then
+        return nil, "not initialized"
+    end
+
+    if not self.state ~= STATE_CONNECTED then
+        sock:send("DISCONNECT\x0d\x0a\x00\x0d\x0a")
+    end
+
+    self.state = nil
+    return sock:close()
+end
 
 
 local class_mt = {
